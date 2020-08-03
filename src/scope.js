1e0c2a4de4bf168e7dee4d2af239757569153784
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-03 23:09:07 +0300

    values based dirty-checking

--- src/scope.js
+++ src/scope.js
@@ -9,34 +9,15 @@ function Scope() {
 
 function initWatchVal() { }
 
-Scope.prototype.$watch = function (watchFn, listenerFn) {
+Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
     var watcher = {
         watchFn: watchFn,
         listenerFn: listenerFn || function () { },
+        valueEq: !!valueEq,
         last: initWatchVal
     };
     this.$$watchers.push(watcher);
     this.$$lastDirtyWatch = null;
-};
-
-Scope.prototype.$$digestOnce = function () {
-    var self = this;
-    var newValue, oldValue, dirty;
-    _.forEach(this.$$watchers, function (watcher) {
-        newValue = watcher.watchFn(self);
-        oldValue = watcher.last;
-        if (newValue !== oldValue) {
-            self.$$lastDirtyWatch = watcher;
-            watcher.last = newValue;
-            watcher.listenerFn(newValue,
-                (oldValue === initWatchVal ? newValue : oldValue),
-                self);
-            dirty = true;
-        } else if (self.$$lastDirtyWatch === watcher) {
-            return false;
-        }
-    });
-    return dirty;
 };
 
 Scope.prototype.$digest = function () {
@@ -49,6 +30,34 @@ Scope.prototype.$digest = function () {
             throw '10 digest iterations reached';
         }
     } while (dirty);
+};
+
+Scope.prototype.$$digestOnce = function () {
+    var self = this;
+    var newValue, oldValue, dirty;
+    _.forEach(this.$$watchers, function (watcher) {
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+            self.$$lastDirtyWatch = watcher;
+            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+            watcher.listenerFn(newValue,
+                (oldValue === initWatchVal ? newValue : oldValue),
+                self);
+            dirty = true;
+        } else if (self.$$lastDirtyWatch === watcher) {
+            return false;
+        }
+    });
+    return dirty;
+};
+
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+    if (valueEq) {
+        return _.isEqual(newValue, oldValue);
+    } else {
+        return newValue === oldValue;
+    }
 };
 
 module.exports = Scope;
