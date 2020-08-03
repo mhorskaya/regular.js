6b448b75507d0eebf7bf43f2fad1802980257f09
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-03 22:48:13 +0300

    get notified of digests

--- src/scope.js
+++ src/scope.js
@@ -11,7 +11,7 @@ function initWatchVal() { }
 Scope.prototype.$watch = function (watchFn, listenerFn) {
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn,
+        listenerFn: listenerFn || function () { },
         last: initWatchVal
     };
     this.$$watchers.push(watcher);
