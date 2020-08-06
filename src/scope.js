c71e093f956750f354674bd796140dde1a118ebe
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-06 11:50:33 +0300

    separate watches

--- src/scope.js
+++ src/scope.js
@@ -238,6 +238,7 @@ Scope.prototype.$new = function () {
     var ChildScope = function () { };
     ChildScope.prototype = this;
     var child = new ChildScope();
+    child.$$watchers = [];
     return child;
 };
 
