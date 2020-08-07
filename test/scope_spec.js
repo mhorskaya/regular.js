3f2f2607c35b9d3efd783d136b5dc795b92de166
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-07 13:32:27 +0300

    substitute parent scope

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1034,5 +1034,21 @@ describe('Scope', function () {
             child.$digest();
             expect(applied).toBe(true);
         });
+
+        it('can take some other scope as the parent', function () {
+            var prototypeParent = new Scope();
+            var hierarchyParent = new Scope();
+            var child = prototypeParent.$new(false, hierarchyParent);
+            prototypeParent.a = 42;
+            expect(child.a).toBe(42);
+            child.counter = 0;
+            child.$watch(function (scope) {
+                scope.counter++;
+            });
+            prototypeParent.$digest();
+            expect(child.counter).toBe(0);
+            hierarchyParent.$digest();
+            expect(child.counter).toBe(2);
+        });
     });
 });
