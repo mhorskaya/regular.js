610baa2a3f5ba5e20e4d52c42dafb5d20a01870e
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-07 13:30:57 +0300

    add isolated scopes

--- src/scope.js
+++ src/scope.js
@@ -44,8 +44,8 @@ Scope.prototype.$digest = function () {
     this.$root.$$lastDirtyWatch = null;
     this.$beginPhase('$digest');
 
-    if (this.$$applyAsyncId) {
-        clearTimeout(this.$$applyAsyncId);
+    if (this.$root.$$applyAsyncId) {
+        clearTimeout(this.$root.$$applyAsyncId);;
         this.$$flushApplyAsync();
     }
 
@@ -173,7 +173,7 @@ Scope.prototype.$$flushApplyAsync = function () {
         }
     }
 
-    this.$$applyAsyncId = null;
+    this.$root.$$applyAsyncId = null;;
 };
 
 Scope.prototype.$applyAsync = function (expr) {
@@ -183,8 +183,8 @@ Scope.prototype.$applyAsync = function (expr) {
         self.$eval(expr);
     });
 
-    if (self.$$applyAsyncId === null) {
-        self.$$applyAsyncId = setTimeout(function () {
+    if (self.$root.$$applyAsyncId === null) {
+        self.$root.$$applyAsyncId = setTimeout(function () {
             self.$apply(_.bind(self.$$flushApplyAsync, self));
         }, 0);
     }
@@ -243,10 +243,19 @@ Scope.prototype.$watchGroup = function (watchFns, listenerFn) {
     };
 };
 
-Scope.prototype.$new = function () {
-    var ChildScope = function () { };
-    ChildScope.prototype = this;
-    var child = new ChildScope();
+Scope.prototype.$new = function (isolated) {
+    var child;
+    if (isolated) {
+        child = new Scope();
+        child.$root = this.$root;
+        child.$$asyncQueue = this.$$asyncQueue;
+        child.$$postDigestQueue = this.$$postDigestQueue;
+        child.$$applyAsyncQueue = this.$$applyAsyncQueue;
+    } else {
+        var ChildScope = function () { };
+        ChildScope.prototype = this;
+        child = new ChildScope();
+    }
     this.$$children.push(child);
     child.$$watchers = [];
     child.$$children = [];
