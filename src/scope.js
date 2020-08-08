39e7e9a77ef83a8aec36e7916aa884709606eb6c
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-08 14:21:04 +0300

    detect new or replaced attributes in objects

--- src/scope.js
+++ src/scope.js
@@ -316,6 +316,13 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
                     changeCount++;
                     oldValue = {};
                 }
+                _.forOwn(newValue, function (newVal, key) {
+                    var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+                    if (!bothNaN && oldValue[key] !== newVal) {
+                        changeCount++;
+                        oldValue[key] = newVal;
+                    }
+                });
             }
         } else {
             if (!self.$$areEqual(newValue, oldValue, false)) {
