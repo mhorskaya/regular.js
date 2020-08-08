c23c688aa906379139d3a9925df0a6a72ec4188a
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-08 13:23:11 +0300

    detect replaced or reordered items in arrays

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1152,7 +1152,7 @@ describe('Scope', function () {
             scope.$digest();
             expect(scope.counter).toBe(2);
         });
-        
+
         it('notices an item removed from an array', function () {
             scope.arr = [1, 2, 3];
             scope.counter = 0;
@@ -1169,6 +1169,55 @@ describe('Scope', function () {
             expect(scope.counter).toBe(2);
             scope.$digest();
             expect(scope.counter).toBe(2);
+        });
+
+        it('notices an item replaced in an array', function () {
+            scope.arr = [1, 2, 3];
+            scope.counter = 0;
+            scope.$watchCollection(
+                function (scope) { return scope.arr; },
+                function (newValue, oldValue, scope) {
+                    scope.counter++;
+                }
+            );
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.arr[1] = 42;
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+        });
+
+        it('notices items reordered in an array', function () {
+            scope.arr = [2, 1, 3];
+            scope.counter = 0;
+            scope.$watchCollection(
+                function (scope) { return scope.arr; },
+                function (newValue, oldValue, scope) {
+                    scope.counter++;
+                }
+            );
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.arr.sort();
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+        });
+
+        it('does not fail on NaNs in arrays', function () {
+            scope.arr = [2, NaN, 3];
+            scope.counter = 0;
+            scope.$watchCollection(
+                function (scope) { return scope.arr; },
+                function (newValue, oldValue, scope) {
+                    scope.counter++;
+                }
+            );
+            scope.$digest();
+            expect(scope.counter).toBe(1);
         });
     });
 });
