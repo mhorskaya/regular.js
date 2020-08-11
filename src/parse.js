4aa78a6e26ddf734c36297bfe28816210fc47736
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-11 10:53:04 +0300

    parse a non-empty array

--- src/parse.js
+++ src/parse.js
@@ -25,7 +25,7 @@ Lexer.prototype.lex = function (text) {
             this.readNumber();
         } else if (this.ch === '\'' || this.ch === '"') {
             this.readString(this.ch);
-        } else if (this.ch === '[' || this.ch === ']') {
+        } else if (this.ch === '[' || this.ch === ']' || this.ch === ',') {
             this.tokens.push({
                 text: this.ch
             });
@@ -175,14 +175,14 @@ AST.prototype.primary = function () {
     if (this.expect('[')) {
         return this.arrayDeclaration();
     } else if (this.constants.hasOwnProperty(this.tokens[0].text)) {
-        return this.constants[this.tokens[0].text];
+        return this.constants[this.consume().text];
     } else {
         return this.constant();
     }
 };
 
 AST.prototype.constant = function () {
-    return { type: AST.Literal, value: this.tokens[0].value };
+    return { type: AST.Literal, value: this.consume().value };
 };
 
 AST.prototype.constants = {
@@ -191,11 +191,19 @@ AST.prototype.constants = {
     'false': { type: AST.Literal, value: false }
 };
 
-AST.prototype.expect = function (e) {
+AST.prototype.peek = function (e) {
     if (this.tokens.length > 0) {
-        if (this.tokens[0].text === e || !e) {
-            return this.tokens.shift();
+        var text = this.tokens[0].text;
+        if (text === e || !e) {
+            return this.tokens[0];
         }
+    }
+};
+
+AST.prototype.expect = function (e) {
+    var token = this.peek(e);
+    if (token) {
+        return this.tokens.shift();
     }
 };
 
@@ -208,8 +216,17 @@ AST.prototype.consume = function (e) {
 };
 
 AST.prototype.arrayDeclaration = function () {
+    var elements = [];
+    if (!this.peek(']')) {
+        do {
+            if (this.peek(']')) {
+                break;
+            }
+            elements.push(this.primary());
+        } while (this.expect(','));
+    }
     this.consume(']');
-    return { type: AST.ArrayExpression };
+    return { type: AST.ArrayExpression, elements: elements };
 };
 
 function ASTCompiler(astBuilder) {
@@ -233,7 +250,10 @@ ASTCompiler.prototype.recurse = function (ast) {
         case AST.Literal:
             return this.escape(ast.value);
         case AST.ArrayExpression:
-            return '[]';
+            var elements = _.map(ast.elements, _.bind(function (element) {
+                return this.recurse(element);
+            }, this));
+            return '[' + elements.join(',') + ']';
     }
 };
 
