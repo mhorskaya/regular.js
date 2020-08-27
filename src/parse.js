b102bb76435e786359f9dc400fed0ff465d54d6c
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-27 15:12:19 +0300

    allow stateful filters

--- src/parse.js
+++ src/parse.js
@@ -1017,7 +1017,8 @@ function markConstantAndWatchExpressions(ast) {
             ast.toWatch = [ast];
             break;
         case AST.CallExpression:
-            allConstants = ast.filter ? true : false;
+            var stateless = ast.filter && !filter(ast.callee.name).$stateful;
+            allConstants = stateless ? true : false;
             argsToWatch = [];
             _.forEach(ast.arguments, function (arg) {
                 markConstantAndWatchExpressions(arg);
@@ -1027,7 +1028,7 @@ function markConstantAndWatchExpressions(ast) {
                 }
             });
             ast.constant = allConstants;
-            ast.toWatch = ast.filter ? argsToWatch : [ast];
+            ast.toWatch = stateless ? argsToWatch : [ast];
             break;
         case AST.AssignmentExpression:
             markConstantAndWatchExpressions(ast.left);
