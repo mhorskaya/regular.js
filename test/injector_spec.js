77bac66ea7d60f5928d1f6c2f7eec4e41a13ef77
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-29 12:22:05 +0300

    add the injector

---
+++ test/injector_spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var setupModuleLoader = require('../src/loader');
+var createInjector = require('../src/injector');
+
+describe('injector', function () {
+    beforeEach(function () {
+        delete window.angular;
+        setupModuleLoader(window);
+    });
+
+    it('can be created', function () {
+        var injector = createInjector([]);
+        expect(injector).toBeDefined();
+    });
+});
