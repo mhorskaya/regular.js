925548ab6d90f4b4c7fb5190f552a7cb371e3d1c
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-29 12:09:12 +0300

    initialize the global just once

--- test/loader_spec.js
+++ test/loader_spec.js
@@ -2,9 +2,20 @@
 
 var setupModuleLoader = require('../src/loader');
 
+beforeEach(function () {
+    delete window.angular;
+});
+
 describe('setupModuleLoader', function () {
     it('exposes angular on the window', function () {
         setupModuleLoader(window);
         expect(window.angular).toBeDefined();
+    });
+
+    it('creates angular just once', function () {
+        setupModuleLoader(window);
+        var ng = window.angular;
+        setupModuleLoader(window);
+        expect(window.angular).toBe(ng);
     });
 });
