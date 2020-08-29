bd6c730d4a985568f71ce2016d8cffb0b8c605fb
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-29 12:12:14 +0300

    add the module method

--- src/loader.js
+++ src/loader.js
@@ -4,8 +4,13 @@ function setupModuleLoader(window) {
     var ensure = function (obj, name, factory) {
         return obj[name] || (obj[name] = factory());
     };
-    
+
     var angular = ensure(window, 'angular', Object);
+
+    ensure(angular, 'module', function () {
+        return function () {
+        };
+    });
 }
 
 module.exports = setupModuleLoader;
--- test/loader_spec.js
+++ test/loader_spec.js
@@ -18,4 +18,16 @@ describe('setupModuleLoader', function () {
         setupModuleLoader(window);
         expect(window.angular).toBe(ng);
     });
+
+    it('exposes the angular module function', function () {
+        setupModuleLoader(window);
+        expect(window.angular.module).toBeDefined();
+    });
+
+    it('exposes the angular module function just once', function () {
+        setupModuleLoader(window);
+        var module = window.angular.module;
+        setupModuleLoader(window);
+        expect(window.angular.module).toBe(module);
+    });
 });
