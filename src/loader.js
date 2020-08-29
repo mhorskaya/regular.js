1641f389b043daa4a07a678e62d085a84d3f39be
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-29 12:19:45 +0300

    get registered modules

--- src/loader.js
+++ src/loader.js
@@ -5,16 +5,32 @@ function setupModuleLoader(window) {
         return obj[name] || (obj[name] = factory());
     };
     var angular = ensure(window, 'angular', Object);
-    var createModule = function (name, requires) {
+    var createModule = function (name, requires, modules) {
+        if (name === 'hasOwnProperty') {
+            throw 'hasOwnProperty is not a valid module name';
+        }
         var moduleInstance = {
             name: name,
             requires: requires
         };
+        modules[name] = moduleInstance;
         return moduleInstance;
     };
+    var getModule = function (name, modules) {
+        if (modules.hasOwnProperty(name)) {
+            return modules[name];
+        } else {
+            throw 'Module ' + name + ' is not available!';
+        }
+    };
     ensure(angular, 'module', function () {
+        var modules = {};
         return function (name, requires) {
-            return createModule(name, requires);
+            if (requires) {
+                return createModule(name, requires, modules);
+            } else {
+                return getModule(name, modules);
+            }
         };
     });
 }
--- test/loader_spec.js
+++ test/loader_spec.js
@@ -52,5 +52,24 @@ describe('setupModuleLoader', function () {
             var myModule = window.angular.module('myModule', ['myOtherModule']);
             expect(myModule.requires).toEqual(['myOtherModule']);
         });
+
+        it('allows getting a module', function () {
+            var myModule = window.angular.module('myModule', []);
+            var gotModule = window.angular.module('myModule');
+            expect(gotModule).toBeDefined();
+            expect(gotModule).toBe(myModule);
+        });
+
+        it('throws when trying to get a nonexistent module', function () {
+            expect(function () {
+                window.angular.module('myModule');
+            }).toThrow();
+        });
+
+        it('does not allow a module to be called hasOwnProperty', function () {
+            expect(function () {
+                window.angular.module('hasOwnProperty', []);
+            }).toThrow();
+        });
     });
 });
