ea669e90938186bf1d4ec8fea5b3fd6fb8477b1f
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-29 16:59:58 +0300

    require other modules

--- src/injector.js
+++ src/injector.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 
 function createInjector(modulesToLoad) {
     var cache = {};
+    var loadedModules = {};
     var $provide = {
         constant: function (key, value) {
             if (key === 'hasOwnProperty') {
@@ -12,13 +13,17 @@ function createInjector(modulesToLoad) {
             cache[key] = value;
         }
     };
-    _.forEach(modulesToLoad, function (moduleName) {
-        var module = window.angular.module(moduleName);
-        _.forEach(module._invokeQueue, function (invokeArgs) {
-            var method = invokeArgs[0];
-            var args = invokeArgs[1];
-            $provide[method].apply($provide, args);
-        });
+    _.forEach(modulesToLoad, function loadModule(moduleName) {
+        if (!loadedModules.hasOwnProperty(moduleName)) {
+            loadedModules[moduleName] = true;
+            var module = window.angular.module(moduleName);
+            _.forEach(module.requires, loadModule);
+            _.forEach(module._invokeQueue, function (invokeArgs) {
+                var method = invokeArgs[0];
+                var args = invokeArgs[1];
+                $provide[method].apply($provide, args);
+            });
+        }
     });
     return {
         has: function (key) {
