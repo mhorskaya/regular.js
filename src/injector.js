d7e7e0dec59e9435b7a88fe1eed35cd9026e6f36
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-30 05:45:04 +0300

    make sure everything is a singleton

--- src/injector.js
+++ src/injector.js
@@ -47,7 +47,8 @@ function createInjector(modulesToLoad, strictDi) {
             return instanceCache[name];
         } else if (providerCache.hasOwnProperty(name + 'Provider')) {
             var provider = providerCache[name + 'Provider'];
-            return invoke(provider.$get, provider);
+            var instance = instanceCache[name] = invoke(provider.$get);
+            return instance;
         }
     }
     function invoke(fn, self, locals) {
