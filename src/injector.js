3762528fd5dfad8fd1428674b7bd86c657a02351
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-08-30 05:18:11 +0300

    add the simplest possible provider

--- src/injector.js
+++ src/injector.js
@@ -16,6 +16,9 @@ function createInjector(modulesToLoad, strictDi) {
                 throw 'hasOwnProperty is not a valid constant name!';
             }
             cache[key] = value;
+        },
+        provider: function (key, provider) {
+            cache[key] = provider.$get();
         }
     };
     function annotate(fn) {
